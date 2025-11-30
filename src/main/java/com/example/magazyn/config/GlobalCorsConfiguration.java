52e9f636e2b97d66c7270b506c917a2a1c74419c
Lexert19 <dominikch19@gmail.com>
2025-11-30 19:17:22 +0100

    cors

---
+++ src/main/java/com/example/magazyn/config/GlobalCorsConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.magazyn.config;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class GlobalCorsConfiguration {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Zezwól na wszystko
+        config.setAllowCredentials(false); // Przy "*" musi być false
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter());
+
+        // TO JEST KLUCZOWE: Ustawiamy najwyższy priorytet
+        // Dzięki temu CORS wykona się zanim Spring Security zablokuje żądanie
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
