a5ef8cdf950c1c01274307651c702699e6fd3e8d
Lexert19 <dominikch19@gmail.com>
2025-11-30 19:44:55 +0100

    cors

--- src/main/java/com/example/magazyn/config/GlobalCorsConfiguration.java
+++ src/main/java/com/example/magazyn/config/GlobalCorsConfiguration.java
@@ -2,8 +2,6 @@ package com.example.magazyn.config;
 
 import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,10 +9,10 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.Collections;
 
-@Configuration
+//@Configuration
 public class GlobalCorsConfiguration {
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean<CorsFilter> customCorsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
